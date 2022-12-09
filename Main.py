14ed168cce7350dc41cd95fbdf84775e14b41fe1
CHO YESEUL <86948867+seul1230@users.noreply.github.com>
2022-12-10 04:41:04 +0900

    resize font

--- Main.py
+++ Main.py
@@ -84,5 +84,6 @@ if uploaded_file is not None:
     fig, ax = plt.subplots(figsize=(1, 1))
     ax.imshow(img)
     ax.set_title(title_predict(uploaded_file)[0])
+    plt.rc('font', size=15)
     plt.axis('off')
     st.pyplot(fig)
