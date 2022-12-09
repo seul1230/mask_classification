4e30e06a1dab91a94c52ff13fe98f91c4e7d7731
CHO YESEUL <86948867+seul1230@users.noreply.github.com>
2022-12-10 05:16:53 +0900

    change cache func

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ st.title('😷 마스크 착용 감지 모델 🙂')
 # 모델 임포트
 
 
-@st.cache(hash_funcs={dict: lambda _: None})
+@st.cache(allow_output_mutation=True)
 def load_model():
     return tf.keras.models.load_model('ResNet152V2_0.9659.h5')
 
