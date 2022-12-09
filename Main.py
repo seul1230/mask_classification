b604a0118cf5c2562dcd5f04dcd58d05103d4657
CHO YESEUL <86948867+seul1230@users.noreply.github.com>
2022-12-10 05:37:56 +0900

    resize lottie

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ lottie_url = "https://assets8.lottiefiles.com/packages/lf20_5q3dohib.json"
 # "https://assets6.lottiefiles.com/private_files/lf30_kbjokjdo.json"
 
 lottie_json = load_lottieurl(lottie_url)
-st_lottie(lottie_json, speed=1, height=300, key="initial")
+st_lottie(lottie_json, speed=1, height=200, key="initial")
 
 st.header("""**오9오9**
 ***MINI project***  
