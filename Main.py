c41fee90d7ddd0e82cfcc21530e8ebe979592d15
CHO YESEUL <86948867+seul1230@users.noreply.github.com>
2022-12-10 10:54:55 +0900

    change lottie

--- Main.py
+++ Main.py
@@ -23,7 +23,8 @@ def load_lottieurl(url: str):
     return r.json()
 
 
-lottie_url = "https://assets8.lottiefiles.com/packages/lf20_5q3dohib.json"
+lottie_url = "https://assets8.lottiefiles.com/packages/lf20_uidhg9jw.json"
+# "https://assets8.lottiefiles.com/packages/lf20_5q3dohib.json"
 # "https://assets6.lottiefiles.com/private_files/lf30_kbjokjdo.json"
 
 lottie_json = load_lottieurl(lottie_url)
